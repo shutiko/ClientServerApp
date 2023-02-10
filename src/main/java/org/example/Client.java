79affe3bb2408ef4f44974c694c34ff61a6a2757
Borblik Oleg <likorff@list.ru>
2023-02-10 16:46:24 +0700

    first commit

---
+++ src/main/java/org/example/Client.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class Client {
+    public static void main(String[] args) {
+        String host = "localhost";
+        int port = 8085;
+        try (
+                Socket clientSocket = new Socket(host, port);
+                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
+            out.println("GET / HTTP/1.1" + " Host: " + host);
+            String resp = in.readLine();
+            System.out.println(resp);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
